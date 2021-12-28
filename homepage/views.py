49f594b7b9fec65c829a3ab2e40a1d0629837d8f
kdemirta <keremdemirtas@gmail.com>
2021-12-28 15:56:54 +0300

    about me page added

--- homepage/views.py
+++ homepage/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Hello, world. You're at the home index.")
 
+def about_me(request):
+    return HttpResponse("<h1>About Me</h1>")
