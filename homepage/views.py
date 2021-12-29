c4e94d9f6a11705f4b2bc3c74515a42058110ea1
kdemirta <keremdemirtas@gmail.com>
2021-12-29 11:42:31 +0300

    template usage example

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Kerem',
+        'title': 'Example Post',
+        'content': 'Example Content',
+        'date_posted': 'December 29, 2021'
+    },
+        {
+        'author': 'Jane Doe',
+        'title': 'Example Post 2',
+        'content': 'Example Content 2',
+        'date_posted': 'December 30, 2021'
+    }
+]
+
 def home(request):
-    return render(request, 'homepage/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'homepage/home.html', context)
 
 def about_me(request):
     return render(request, 'homepage/about.html')
