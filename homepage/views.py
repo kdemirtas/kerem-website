18003077ad3851e4f7dc43a8aa92bb9230b7482f
kdemirta <keremdemirtas@gmail.com>
2022-01-06 12:23:09 +0300

    use the Post table instead of dummy data

--- homepage/views.py
+++ homepage/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'Kerem',
-        'title': 'Example Post',
-        'content': 'Example Content',
-        'date_posted': 'December 29, 2021'
-    },
-        {
-        'author': 'Jane Doe',
-        'title': 'Example Post 2',
-        'content': 'Example Content 2',
-        'date_posted': 'December 30, 2021'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'homepage/home.html', context)
 
